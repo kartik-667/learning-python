1517c2094f7587091c0954373c6357b8a50ca769
Kartik Verma <vermakartik667@gmail.com>
2025-08-25 11:16:41 +0530

    started learning python, functions and data types

---
+++ file1.py
@@ -0,0 +1,83 @@
+
+
+import math
+import random
+
+randval=random.random() * 10
+randval=math.floor(randval)
+print(randval)
+# print(math.factorial(12))
+
+
+def func1():
+    print("this is fnc 1")
+
+
+def check_sorted(arr):
+
+    if(len(arr)<=1): return True
+
+    for i in range(1,len(arr)):
+        if(arr[i-1] > arr[i]):
+            return False
+
+    return True
+
+def binary_search(arr,val):
+    if(check_sorted(arr)==False): # in case not sorted, so sort it
+         arr.sort()
+        
+    left=0
+    right=len(arr)-1
+
+    while(left<=right):
+        mid=(left + right) //2
+        if(val==arr[mid]):
+            print("val found")
+            return
+        elif(val < arr[mid]):
+            right=mid-1
+        else:
+            left=mid+1
+    
+    print("val not found")
+
+def dfs_graph(node,vis,adj):
+
+    vis.append(node)
+    print(node)
+
+    for adjn in adj[node]:
+        if adjn not in vis:
+            dfs_graph(adjn,vis,adj)
+
+
+    # here it will backtack
+
+def graph_main():
+    adj=[[1,2],[4,5],[5,6]]
+    vis=[]
+    
+    
+    dfs_graph(0,vis,adj)
+
+    print("function completed here")
+    
+
+name="kartikkkaskdkasdkakskdaksksd"
+count=0
+
+for i in name:
+    if(i=='k'): count+=1
+
+
+# print("enter your name")
+# # name2=input()
+
+# print("the inputed name is",name2)
+# print(count,end=" ")
+
+# list1=[]
+# list1.append(1)
+
+# print(list1)
---
+++ file2.py
@@ -0,0 +1,15 @@
+from file1 import func1, binary_search, graph_main
+import os
+import sys
+
+graph_main()
+
+
+# print(sys.platform)
+# tmp=os.getcwd()
+# print(tmp)
+# arr=[1,2,3,4,5,6,7,8,9,10]
+# binary_search(arr,8)
+
+# print("calling fnc")
+# func1()
