63c0403c0e96804b3168b8e3474ee26effd06601
Kartik Verma <vermakartik667@gmail.com>
2025-08-29 23:25:31 +0530

    string learning

--- string1.py
+++ string1.py
@@ -1,6 +1,8 @@
+import math
+import random
 a="kartik k k k k k k k kk verma is gonna boom bro"
 list1=a.split(' ') # eg split by space 
-print(list1)
+# print(list1)
 
 hmap={}
 
@@ -11,6 +13,13 @@ for char in a:
     else:
         hmap[char]=1
 
-print(hmap)
+# print(hmap)
 
-print(f'the fstring is {hmap}') # this is how we use placeholder
+# print(f'the fstring is {hmap}') # this is how we use placeholder
+
+list2=[1,2,3,4,5,6]
+tofind=math.floor(random.random() *10)
+if tofind in list2:
+    print(f" {tofind} value found")
+else:
+    print(f" {tofind} value not found")
